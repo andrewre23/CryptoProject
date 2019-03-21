89b909f18beca700cf87c65bfe0d010a02ad7803
andrewedmonds <andrewre@umich.edu>
2019-03-20 23:47:07 -0400

    added example code of previous NNs

--- notebooks/modeling/keras/layers/__init__.py
+++ notebooks/modeling/keras/layers/__init__.py
@@ -1,6 +1,13 @@
-
+#
+# Based off project at
+# https://github.com/kenoma/KerasFuzzy
+#
+# Implemented per description in
+# Leng, Prasad, McGinnity (2004)
+#
+# Andrew Edmonds - 2019
+#
 
 from .FuzzyLayer import FuzzyLayer
-from .DefuzzyLayer import DefuzzyLayer
 from .NormalizeLayer import NormalizedLayer
 from .WeightedLayer import WeightedLayer
