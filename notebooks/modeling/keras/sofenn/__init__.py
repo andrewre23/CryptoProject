b260224b177db718851c070c711cfead31ce2e20
andrewedmonds <andrewre@umich.edu>
2019-03-22 21:59:16 -0400

    updated comments to add package name

--- notebooks/modeling/keras/sofenn/__init__.py
+++ notebooks/modeling/keras/sofenn/__init__.py
@@ -1,3 +1,19 @@
-# importing sofenn class for package
+#
+# SOFENN
+# Self-Organizing Fuzzy Neural Network
+#
+# (sounds like soften)
+#
+#
+# Implemented per description in
+# An on-line algorithm for creating self-organizing
+# fuzzy neural networks
+# Leng, Prasad, McGinnity (2004)
+#
+#
+# Andrew Edmonds - 2019
+# github.com/andrewre23
+#
+
 
 from .SOFNN import SOFNN
--- notebooks/modeling/keras/sofenn/layers/DefuzzyLayer.py
+++ notebooks/modeling/keras/sofenn/layers/DefuzzyLayer.py
@@ -1,9 +1,15 @@
 #
-# Based off project at
-# https://github.com/kenoma/KerasFuzzy
+# SOFENN
+# Self-Organizing Fuzzy Neural Network
+#
+# (sounds like soften)
+#
 #
 # Implemented per description in
+# An on-line algorithm for creating self-organizing
+# fuzzy neural networks
 # Leng, Prasad, McGinnity (2004)
+#
 #
 # Andrew Edmonds - 2019
 # github.com/andrewre23
