c4c1c604eddfa536378d44ebd4e17daa119aba16
andrewre23 <andrewre@umich.edu>
2019-03-29 19:23:08 -0400

    updated docstring and added build-train-predict functions

--- notebooks/modeling/keras/sofenn/SOFNN.py
+++ notebooks/modeling/keras/sofenn/SOFNN.py
@@ -22,6 +22,8 @@ from keras import backend as K
 from keras.models import Model
 from keras.layers import Input, Dense, Activation
 
+from sklearn.metrics import confusion_matrix, classification_report
+
 # custom Fuzzy Layers
 from .layers import \
     FuzzyLayer, NormalizedLayer, WeightedLayer
@@ -29,7 +31,7 @@ from .layers import \
 
 class SOFNN(object):
     """
-    Class for building Self-Organizing Fuzzy Neural Network
+    Self-Organizing Fuzzy Neural Network
     =======================================================
 
     -Implemented per description in:
@@ -37,6 +39,18 @@ class SOFNN(object):
         fuzzy neural networks" - Leng, Prasad, McGinnity (2004)
     -Composed of 5 layers with varying "fuzzy rule" nodes
 
+    * = samples
+
+    Parameters
+    ==========
+    - X_train : training input data
+        - shape :(train_*, features)
+    - X_test  : testing input data
+        - shape: (test_*, features)
+    - y_train : training output data
+        - shape: (train_*,)
+    - y_test  : testing output data
+        - shape: (test_*,)
 
     Layers
     ======
@@ -71,19 +85,6 @@ class SOFNN(object):
         - input shape  : (*, neurons)
         - output shape : (*,)
 
-    * = samples
-
-    Parameters
-    ==========
-    - X_train : training input data
-        - shape :(train_*, features)
-    - X_test  : testing input data
-        - shape: (test_*, features)
-    - y_train : training output data
-        - shape: (train_*,)
-    - y_test  : testing output data
-        - shape: (test_*,)
-
     Functions
     =========
     - system_error_check :
@@ -95,12 +96,78 @@ class SOFNN(object):
         - add one neuron to model
     - prune_neuron :
         - remove neuron from model
-
-
+    - combine_membership_functions :
+        - combine similar membership functions
     """
 
-    def __init__(self, X_train, X_test, y_train, y_test):
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
+    def __init__(self, X_train, X_test, y_train, y_test, neurons=1):
+
+        # set data attributes
+        self._X_train = X_train
+        self._X_test = X_test
+        self._y_train = y_train
+        self._y_test = y_test
+        # set initial number of neurons
+        self._neurons = neurons
+
+        # build model on init
+        self._model = self._build_model()
+
+    def _build_model(self):
+        """
+        Create and compile model
+        """
+
+        print('Building SOFNN with {} neurons'.format(self._neurons))
+
+        # get shape of training data
+        samples, feats = self._X_train.shape
+
+        # add layers
+        inputs = Input(name='Inputs', shape=(feats,))
+        fuzz = FuzzyLayer(self._neurons)
+        norm = NormalizedLayer(self._neurons)
+        weights = WeightedLayer(self._neurons)
+
+        # run through layers
+        phi = fuzz(inputs)
+        psi = norm(phi)
+        f = weights([inputs, psi])
+        raw_output = Dense(1, name='RawOutput', activation='linear', use_bias=False)(f)
+        preds = Activation(name='OutputActivation', activation='relu')(raw_output)
+
+        # compile model and output summary
+        model = Model(inputs=inputs, outputs=preds)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'mape'])
+        print(model.summary())
+
+        return model
+
+    def train_model(self, epochs=50, batch_size=None):
+        """
+        Run currently saved model
+        """
+        # fit model and evaluate
+        self._model.fit(self._X_train, self._y_train,
+                        epochs=epochs, batch_size=batch_size, shuffle=False)
+
+    def evaluate_model(self, threshold=0.5):
+        """
+        Evaluate currently trained model
+        """
+        scores = self._model.evaluate(self._X_test, self._y_test, verbose=1)
+        accuracy = scores[1] * 100
+        print("\nAccuracy: {:.2f}%".format(accuracy))
+
+        # print confusion matrix
+        print('\nConfusion Matrix')
+        print('=' * 20)
+        y_pred = np.squeeze(np.where(
+            self._model.predict(self._X_test) >= threshold, 1, 0), axis=-1)
+        print(pd.DataFrame(confusion_matrix(self._y_test, y_pred),
+                           index=['true:no', 'true:yes'], columns=['pred:no', 'pred:yes']))
+
+        # print classification report
+        print('\nClassification Report')
+        print('=' * 20)
+        print(classification_report(self._y_test, y_pred, labels=[0, 1]))
